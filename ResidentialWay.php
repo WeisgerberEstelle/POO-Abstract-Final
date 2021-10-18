147a30dc49be7fd8e0bcfa52791c560cf984f292
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2021-10-18 09:50:38 +0200

    Update ResidentialWay.php

--- ResidentialWay.php
+++ ResidentialWay.php
@@ -7,4 +7,9 @@ final class ResidentialWay extends HighWay
     private int $nbLane = 2;
 
     private int $maxSpeed = 50;
+    
+    public function addVehicle(Vehicle $vehicle)
+    {
+            $this->currentVehicles[] = $vehicle;        
+    }
 }
